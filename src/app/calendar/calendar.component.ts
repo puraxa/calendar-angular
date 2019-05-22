69a5ef654effa51ee5123784f4703985f26cac8b
puraxa <edinpurivatra@gmail.com>
2019-05-22 14:50:08 +0200

    removed unused code and css

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -13,7 +13,7 @@ export class CalendarComponent implements OnInit {
     this.load();
   }
   months: Array<string> = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  days: Array<string> = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sat'];
+  days: Array<string> = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sat'];
   today:Date;
   @Input()
   Month:number;
@@ -25,7 +25,6 @@ export class CalendarComponent implements OnInit {
     this.today = new Date();
     this.Month = this.today.getMonth();
     this.Year = this.today.getFullYear();
-    console.log(this.calendar);
     this.displayCalendar();
 }
 isToday(date):boolean{
@@ -36,24 +35,14 @@ isToday(date):boolean{
     let daysInMonth:number = this.getDaysInMonth();
     let firstDay:number = new Date(this.Year,this.Month).getDay();
     for(let i = 0; i < firstDay; i++){
-        //calendar += `<div class="days"></div>`;
         this.calendar.push("");
     }
     for(let i = 0; i < daysInMonth; i++){
         this.calendar.push((i+1).toString());
-        // if(this.today.getMonth() == this.Month && this.today.getDate() == i+1 && this.today.getFullYear() == this.Year){
-        //     calendar += `<div class="days today">${i+1}</div>`;
-        // }
-        // else{
-        //     calendar += `<div class="days">${i+1}</div>`;
-        // }
     }
     for(let i = 0; i < 6 - new Date(this.Year,this.Month,daysInMonth).getDay(); i++){
         this.calendar.push("");
-        //calendar += `<div class="days"></div>`;
     }
-    //document.getElementById('calendar').innerHTML = calendar;
-    //document.getElementById('displaying-month-year').innerText = `${this.months[this.Month]}, ${this.Year}`;
 }
 
  getDaysInMonth():number{
