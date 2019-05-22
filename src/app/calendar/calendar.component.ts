f818fa27a81caf4704cea3e61706a510e9c40169
puraxa <edinpurivatra@gmail.com>
2019-05-22 14:16:29 +0200

    enabled 2 way data binding, today not marked still

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -15,48 +15,43 @@ export class CalendarComponent implements OnInit {
   months: Array<string> = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   days: Array<string> = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sat'];
   today:Date;
+  @Input()
   Month:number;
+  @Input()
   Year:number;
+  calendar: Array<any> = [];
+
  load(){
     this.today = new Date();
     this.Month = this.today.getMonth();
     this.Year = this.today.getFullYear();
-    document.getElementById('displaying-month-year').innerText = `${this.months[this.Month]}, ${this.Year}`;
+    console.log(this.calendar);
     this.displayCalendar();
-    for(let i = 0; i < 12; i++){
-        document.getElementById('month').innerHTML += `
-            <option value=${i}>${this.months[i]}</option>
-        `;
-    }
 }
 
  displayCalendar():void {
+    this.calendar.length = 0;
     let daysInMonth:number = this.getDaysInMonth();
     let firstDay:number = new Date(this.Year,this.Month).getDay();
-    let calendar:string = `
-        <div class="days days-text gray">Su</div>
-        <div class="days days-text gray">Mo</div>
-        <div class="days days-text gray">Tu</div>
-        <div class="days days-text gray">We</div>
-        <div class="days days-text gray">Th</div>
-        <div class="days days-text gray">Fr</div>
-        <div class="days days-text gray">Sa</div>`;
     for(let i = 0; i < firstDay; i++){
-        calendar += `<div class="days"></div>`;
+        //calendar += `<div class="days"></div>`;
+        this.calendar.push("");
     }
     for(let i = 0; i < daysInMonth; i++){
-        if(this.today.getMonth() == this.Month && this.today.getDate() == i+1 && this.today.getFullYear() == this.Year){
-            calendar += `<div class="days today">${i+1}</div>`;
-        }
-        else{
-            calendar += `<div class="days">${i+1}</div>`;
-        }
+        this.calendar.push((i+1).toString());
+        // if(this.today.getMonth() == this.Month && this.today.getDate() == i+1 && this.today.getFullYear() == this.Year){
+        //     calendar += `<div class="days today">${i+1}</div>`;
+        // }
+        // else{
+        //     calendar += `<div class="days">${i+1}</div>`;
+        // }
     }
     for(let i = 0; i < 6 - new Date(this.Year,this.Month,daysInMonth).getDay(); i++){
-        calendar += `<div class="days"></div>`;
+        this.calendar.push("");
+        //calendar += `<div class="days"></div>`;
     }
-    document.getElementById('calendar').innerHTML = calendar;
-    document.getElementById('displaying-month-year').innerText = `${this.months[this.Month]}, ${this.Year}`;
+    //document.getElementById('calendar').innerHTML = calendar;
+    //document.getElementById('displaying-month-year').innerText = `${this.months[this.Month]}, ${this.Year}`;
 }
 
  getDaysInMonth():number{
@@ -73,13 +68,5 @@ export class CalendarComponent implements OnInit {
   this.Month = this.Month+1 > 11 ? 0 : this.Month+1;
   this.Year = this.Month == 0  ? this.Year+1 : this.Year;
   this.displayCalendar();
-}
-
- setDate():void{
-    let m:string = (<HTMLInputElement>document.getElementById('month')).value;
-    this.Month = parseInt(m);
-    let y:string = (<HTMLInputElement>document.getElementById('year')).value;
-    this.Year = parseInt(y);
-    this.displayCalendar();
 }
 }
